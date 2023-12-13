d0e0b4a3ce5789c39605be91f7a41128e686eadb
Leknes <leonhardsenkel@gmail.com>
2023-12-13 20:36:10 +0100

    created library

---
+++ API/src/IStopwatch.cs
@@ -0,0 +1,32 @@
+﻿namespace Senkel.Unity.API;
+
+/// <summary>
+/// Represents a stopwatch that can be started, stopped and reset.
+/// </summary>
+public interface IStopwatch
+{
+    /// <summary>
+    /// The total time measured by this stopwatch.
+    /// </summary>
+    public double Elapsed { get; }
+
+    /// <summary>
+    /// Starts or resumes the measurement of the stopwatch.
+    /// </summary>
+    public void Start();
+
+    /// <summary>
+    /// Stops the measurement of the stopwatch, but should not reset the <see cref="Elapsed"/> value.
+    /// </summary>
+    public void Stop();
+
+    /// <summary>
+    /// Stops the measurement of the stopwatch and resets the <see cref="Elapsed"/> value.
+    /// </summary>
+    public void Reset();
+
+    /// <summary>
+    /// Resets the <see cref="Elapsed"/> value and restarts the stopwatch.
+    /// </summary>
+    public void Restart();
+}
