cf5495e194b8d956af479b198073eefaa2e2b871
Leknes <leonhardsenkel@gmail.com>
2023-12-31 13:04:09 +0100

    Did build

--- Diagnostics/obj/Debug/netstandard2.1/Senkel.Unity.Diagnostics.AssemblyInfo.cs
+++ Diagnostics/obj/Debug/netstandard2.1/Senkel.Unity.Diagnostics.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Leonhard Senkel")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Senkel.Unity.Diagnostics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
